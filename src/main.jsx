6975fb547b16f8686c2c0835ded04af5268d1be8
Arina Zhukova <Azhukova19842005@yandex.ru>
2026-02-04 15:39:05 +0300

    The dependencies are installed and the application architecture is configured

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
 
-createRoot(document.getElementById('root')).render(
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <h1 className="text-blue-600 dark:text-sky-400">Hello world!</h1>
   </StrictMode>,
-)
+);
